c1645917d8ca44a731c0717a079ed2f69df52d59
eldiar644@gmail.com <eldiar6442003@gmail.com>
2021-05-13 14:38:20 +0600

    Выполнил 1 ДЗ

--- app/src/main/java/com/example/taskapp31/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/taskapp31/ui/profile/ProfileFragment.java
@@ -1,28 +1,74 @@
 package com.example.taskapp31.ui.profile;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.taskapp31.R;
 import com.example.taskapp31.ui.dashboard.DashboardViewModel;
+
+import java.io.IOException;
+import java.net.URI;
 
 
 public class ProfileFragment extends Fragment {
 
+    ImageView img;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        img = view.findViewById(R.id.image_view);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/");
+                launchSomeActivity.launch(intent);
+            }
+        });
+
+    }
+    ActivityResultLauncher<Intent> launchSomeActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri selectedImage = data.getData();
+                        img.setImageURI(selectedImage);
+                    }
+                }
+            });
+
+
 }
